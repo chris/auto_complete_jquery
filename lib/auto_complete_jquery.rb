cae3ea433966184fa0f27ff9372d09ee935958d3
Gun.io Whitespace Robot <contact@gun.io>
2011-10-27 11:10:35 -0400

    Remove whitespace [Gun.io WhitespaceBot]

--- lib/auto_complete_jquery.rb
+++ lib/auto_complete_jquery.rb
@@ -1,5 +1,5 @@
-module AutoCompleteJquery      
-  
+module AutoCompleteJquery
+
   def self.included(base)
     base.extend(ClassMethods)
   end
@@ -17,34 +17,34 @@ module AutoCompleteJquery
   #
   # By default, auto_complete_for limits the results to 10 entries,
   # and sorts by the given field.
-  # 
+  #
   # auto_complete_for takes a third parameter, an options hash to
   # the find method used to search for the records:
   #
   #   auto_complete_for :post, :title, :limit => 15, :order => 'created_at DESC'
   #
-  # For help on defining text input fields with autocompletion, 
+  # For help on defining text input fields with autocompletion,
   # see ActionView::Helpers::JavaScriptHelper.
   #
-  # For more on jQuery auto-complete, see the docs for the jQuery autocomplete 
+  # For more on jQuery auto-complete, see the docs for the jQuery autocomplete
   # plugin used in conjunction with this plugin:
   # * http://www.dyve.net/jquery/?autocomplete
   module ClassMethods
     def auto_complete_for(object, method, options = {})
       define_method("auto_complete_for_#{object}_#{method}") do
         object_constant = object.to_s.camelize.constantize
-        
-        find_options = { 
-          :conditions => [ "LOWER(#{method}) LIKE ?", '%' + params[:q].downcase + '%' ], 
+
+        find_options = {
+          :conditions => [ "LOWER(#{method}) LIKE ?", '%' + params[:q].downcase + '%' ],
           :order => "#{method} ASC",
           :select => "#{object_constant.table_name}.#{method}",
           :limit => 10 }.merge!(options)
-        
+
         @items = object_constant.find(:all, find_options).collect(&method)
 
         render :text => @items.join("\n")
       end
     end
   end
-  
+
 end
